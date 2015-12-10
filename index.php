763d37f045dfc7342ab37ee90994d9a6049c91bc
lenerdz <gerardoccf@gmail.com>
2015-12-10 16:13:45 -0300

    Set icons

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	<table>
 	<?php
 		//$clientes = DBRead('ncards','where Npower > 10', 'Nname');
-		$clientes = DBPureQuery("SELECT Nname, Nset, Npower FROM Ncards where Npower > 10");
+		$clientes = DBPureQuery("SELECT Nname, Ncode, Ncode_magiccards FROM Nsets where 1");
 		
 		echo "<tr>";
 		foreach ($clientes[1] as $key => $value) echo "<th>".$key."</th>";
@@ -26,7 +26,16 @@
 		for ($i=0; $i < count($clientes); $i++) { 
 			echo "<tr>";
 			foreach ($clientes[$i] as $key => $value) {
-				echo "<td>".$value."</td>";
+				if($key=="Ncode_magiccards"){
+					if (strpos("teste 15ANN AT ARC ARENA APAC BR BD UQC CP CLASH CSTD DM 8EB EURO FNMP GPX GURU HHO JR DCILM MGDC ORI MPRP MPL MBP PS PTC PRO REP ST ST2K SUM SUS TSTS THGT VMA WMCQ QRL GRC", $value) and !strpos("TESTE AP AN ARCS CS 8E FVE FVL GP PR SH TS US", $value)) {
+						$rarity = "S";
+					}else{
+						$rarity = "R";
+					}
+					echo "<td><img src='http://www.ligamagic.com.br/images/emot/ed/".$value."_$rarity.gif' alt=''>$value</td>";
+				}else{
+					echo "<td>".$value."</td>";
+				}
 			}
 			echo "</tr>";
 		}
--- search.php
+++ search.php
@@ -2,15 +2,33 @@
 	require 'database.php';
 	@$card = $_GET['card'];		//Consertar
 	if(!$card) $card = "Garfield";
-	$clientes = DBLike("Nname", $card);
+	$clientes = DBLike("Nname, Nset", $card);
 ?>
 <html>
 <head>
 	<title>Teste de Conexão MySQLi</title>
 	<meta charset="utf-8">
 	<style>
-
-	</style>
+        tr:hover{
+            background-color: rgba(178, 255, 148, 0.500);
+        }
+        td>span {
+            position: absolute;
+            background-color: lightyellow;
+            padding: 5px;
+            left: -1000px;
+            border: 1px dashed gray;
+            visibility: hidden;
+            color: black;
+            text-decoration: none;
+        }
+        td:hover>span {
+            visibility: visible;
+            //top: 0;
+            left: 0;
+            z-index: 50;
+        }
+    </style>
 </head>
 <body>
 	<form action="search.php" method="get">
@@ -32,7 +50,8 @@
 		for ($i=0; $i < count($clientes); $i++) { 
 			echo "<tr>";
 			foreach ($clientes[$i] as $key => $value) {
-				echo "<td>".$value."</td>";
+				$nome = str_replace(' ', '', $value);
+				echo "<td>".$value."<span><img src=\"http://static.starcitygames.com/sales/cardscans/MTG/JUD/en/nonfoil/TestOfEndurance.jpg\"</span></td>";
 			}
 			echo "</tr>";
 		}
